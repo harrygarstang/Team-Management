6956e10ac35207181af860c2d7f4e6a381dcfee1
harryg <harry@gmail.com>
2023-02-27 16:33:15 +0000

    Added outlines for functions to acquire name, ID, email, and role of employee within the class

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -5,6 +5,22 @@ class Employee {
         this.id = id;
         this.email = email;
     }
-}
+    getName() {
+
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getRole() {
+        return Employee;
+    }
+};
+
 
 module.exports = Employee;
